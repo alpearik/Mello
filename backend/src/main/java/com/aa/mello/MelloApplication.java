996628aa075caf5ddd9a7f6dc548dabc43f4e2be
Alperen <ayaako09002@gmail.com>
2025-08-19 16:03:28 +0200

    Initial commit: Spring Boot setup

---
+++ backend/src/main/java/com/aa/mello/MelloApplication.java
@@ -0,0 +1,13 @@
+package com.aa.mello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelloApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aa/mello/MelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa.mello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
